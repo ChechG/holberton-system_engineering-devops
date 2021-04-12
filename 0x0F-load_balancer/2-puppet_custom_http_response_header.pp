0d87c12a25690125b883d31a17408bba7d717b9c
ChechG <2100@holbertonschool.com>
2021-04-12 23:58:51 +0000

    2

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,7 +1,7 @@
 # Install nginx with puppet
 
-exec { 'puppet_header':
-    command  =>  'sudo apt-get -y update; sudo apt-get -y install nginx;
-                sudo sed -i "46i\add_header X-Served-By $HOSTNAME ;" /etc/nginx/sites-available/default'
-    provider =>  shell,
+exec {'puppet_header':
+    command  =>  'sudo apt-get update -y; sudo apt-get install nginx -y;
+                  sudo sed -i "46i\add_header X-Served-By \$HOSTNAME ;" /etc/nginx/sites-available/default'
+    provider =>   shell,
 }
