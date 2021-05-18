7413702028a90c65c9f5928b212bbd021b1fcce5
ChechG <2100@holbertonschool.com>
2021-05-18 18:04:33 +0000

    pep8 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,15 +7,18 @@ import requests
 import requests.auth
 
 
-
 def number_of_subscribers(subreddit):
     base_url = 'https://www.reddit.com/'
-    data = {'grant_type': 'password', 'username': 'ChechG', 'password': 'Au7kiul8l'}
-    auth = requests.auth.HTTPBasicAuth('u-ff2BP3dLBRIg', 'Hm0PSCGrsPGC0G5WNYWJHnP-hC8--A')
+    u = 'ChechG'
+    p = 'Au7kiul8l'
+    u_id = 'u-ff2BP3dLBRIg'
+    s_key = 'Hm0PSCGrsPGC0G5WNYWJHnP-hC8--A'
+    data = {'grant_type': 'password', 'username': u, 'password': p}
+    auth = requests.auth.HTTPBasicAuth(u_id, s_key)
     r = requests.post(base_url + 'api/v1/access_token',
-                    data=data,
-                    headers={'user-agent': 'chechApp'},
-            auth=auth)
+                      data=data,
+                      headers={'user-agent': 'chechApp'},
+                      auth=auth)
     d = r.json()
     token = 'bearer ' + d['access_token']
 
