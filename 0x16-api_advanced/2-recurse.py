a1a12f043532091d3e3bd5423a7cf77314f809ec
ChechG <2100@holbertonschool.com>
2021-05-18 20:43:00 +0000

    2

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -9,4 +9,35 @@ import requests.auth
 
 
 def recurse(subreddit, hot_list=[]):
+    base_url = 'https://www.reddit.com/'
+    u = 'ChechG'
+    p = 'liverpool'
+    u_id = 'u-ff2BP3dLBRIg'
+    s_key = 'Hm0PSCGrsPGC0G5WNYWJHnP-hC8--A'
+    data = {'grant_type': 'password', 'username': u, 'password': p}
+    auth = requests.auth.HTTPBasicAuth(u_id, s_key)
+    r = requests.post(base_url + 'api/v1/access_token',
+                      data=data,
+                      headers={'user-agent': 'chechApp'},
+                      auth=auth)
+    d = r.json()
+    token = 'bearer ' + d['access_token']
+
+    base_url = 'https://oauth.reddit.com'
+
+    heads = {'Authorization': token, 'User-Agent': 'chechApp'}
+    response = requests.get(base_url + '/r/' + subreddit + '/hot', headers=heads)
     
+    if response:
+        dict_u = response.json()
+        length = len(hot_list)
+        dato = dict_u['data']['children'][length]['data']['title']
+        len_list = len(dict_u['data']['children'])
+        if length <= len_list - 1:
+            if dato not in hot_list:
+                hot_list.append(dato)
+                if length < len_list - 1:
+                    recurse(subreddit, hot_list)
+        return hot_list
+    else:
+        print("None")
