66385ebb65e76ef4c0acd332ceb033962715d7c2
ChechG <2100@holbertonschool.com>
2021-05-19 19:11:20 +0000

    0

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,5 @@
 # fin error and fix it ith puppet
 exec {
-    command  => 'sudo sed -i "s\define('WP_DEBUG', false);\define('WP_DEBUG', true);" /var/www/html/wp-config;
-                sudo sed -i "s\phpp\p" /var/www/html/wp-settings.php',
+    command  => 'sudo sed -i "s\phpp\p" /var/www/html/wp-settings.php',
     provider =>  shell,
 }
